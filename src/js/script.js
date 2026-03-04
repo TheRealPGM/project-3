0f2ecc1cdeffa62787468892c34d65f102cf2096
Alexander <prlmglvngmzg@gmail.com>
2026-03-04 20:25:21 +0200

    04/03/2026(1)

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,47 @@
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 // import "purecss/build/grids-min.css";
 // import "purecss/build/grids-responsive-min.css";
 
 import "/src/sass/style.scss";
+
+try {
+    // init Swiper:
+    new Swiper('.works__slider', {
+        // Default parameters
+        slidesPerView: 1,
+        loop: true,
+
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+
+        modules: [Navigation, Pagination],
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // Responsive breakpoints
+        breakpoints: {
+        // when window width is >= 1200px
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 5,
+            },
+
+            1920: {
+                spaceBetween: 35,
+            },
+        },
+    });
+} catch (e) {}
